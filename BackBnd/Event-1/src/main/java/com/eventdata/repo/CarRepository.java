a8d0fc4df6405019d3bbead77df7620ed8c94e55
Helen Wubneh <helina123konjo@gmail.com>
2022-03-18 08:08:22 -0700

    change

--- BackBnd/Event-1/src/main/java/com/eventdata/repo/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventdata.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eventdata.model.Car;
-
-public interface CarRepository extends JpaRepository<Car,Integer> {
-
-}
--- BackBnd/JSOUP/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author Helen Wubneh
- *
- */
-module JSOUP {
-}
